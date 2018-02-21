ef45589d2f9cdb6b1697c4c0b35425eca4b15002
Taylor Denouden <taylor.denouden@hakai.org>
2018-02-21 12:00:06 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as file:
     long_description = file.read()
 
 setup(name='Deep-Models',
-      version='0.0.1',
+      version='0.0.2',
       description='Keras deep learning model implementations',
       long_description=long_description,
       url='http://github.com/tayden/DeepModels',
