ef68c34acb045dfae5581bcf5e6938e43097974c
Taylor Denouden <taylor.denouden@hakai.org>
2018-02-21 11:59:12 -0500

    Update long_description on pypi

--- setup.py
+++ setup.py
@@ -2,9 +2,13 @@
 
 from setuptools import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
 setup(name='Deep-Models',
       version='0.0.1',
       description='Keras deep learning model implementations',
+      long_description=long_description,
       url='http://github.com/tayden/DeepModels',
       author='Taylor Denouden',
       author_email='taylordenouden@gmail.com',
